d7ffc32cd29de1440bdf5c7129a61a6dbc47aab6
Paul Sonnentag <paul.sonnentag@googlemail.com>
2023-12-15 09:51:46 +0100

    Add automerge-repo-demo

---
+++ demo-automerge-repo/src/App.tsx
@@ -0,0 +1,9 @@
+import { AuthProvider } from '../../packages/auth-provider-automerge-repo/dist/index'
+
+console.log(AuthProvider)
+
+function App() {
+  return 'hello world'
+}
+
+export default App
