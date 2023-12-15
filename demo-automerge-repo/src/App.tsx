b3d8c30600570761c8b2c717490fbdff97a1171f
Paul Sonnentag <paul.sonnentag@googlemail.com>
2023-12-15 11:09:04 +0100

    WIP: two users

--- demo-automerge-repo/src/App.tsx
+++ demo-automerge-repo/src/App.tsx
@@ -4,45 +4,74 @@ import { Repo } from '@automerge/automerge-repo'
 import { BrowserWebSocketClientAdapter } from '@automerge/automerge-repo-network-websocket'
 import { DummyStorageAdapter } from './DummyStorageAdapter'
 
-const storage = new DummyStorageAdapter()
+const alice = ((window as any).alice = await createUser('alice'))
+const bob = ((window as any).bob = await createUser('bob'))
 
-const userName = 'bob'
-const user = Auth.createUser(userName)
-const { userId } = user
-const device = Auth.createDevice(userId, `${userName}'s device`)
-const context = { user, device }
+interface CounterDoc {
+  counter: number
+}
 
-const authProvider = new AuthProvider({ user, device, storage })
+const aliceHandle = alice.repo.create<CounterDoc>()
 
-const repo = new Repo({
-  network: [authProvider.wrap(new BrowserWebSocketClientAdapter('ws://localhost:3030'))],
-  storage,
+aliceHandle.change(doc => {
+  doc.counter = 100
 })
 
-repo.networkSubsystem.on('peer', payload => {
-  console.log('new peer', payload)
+const bobHandle = bob.repo.find<CounterDoc>(aliceHandle.url)
+
+bobHandle.doc().then(doc => {
+  console.log(doc)
 })
 
-// create a team
-const team = Auth.createTeam('team A', context)
-await authProvider.addTeam(team)
+//localStorage.setItem('debug', 'localfirst:auth*')
 
-// get the server's public keys
-const response = await fetch(`http://localhost:3030/keys`)
-const keys = await response.json()
+async function createUser(name: string) {
+  const storage = new DummyStorageAdapter()
 
-// add the server's public keys to the team
-team.addServer({ host: 'localhost', keys })
+  const user = Auth.createUser(name)
+  const { userId } = user
+  const device = Auth.createDevice(userId, `${name}'s device`)
+  const context = { user, device }
 
-// register the team with the server
-await fetch(`http://localhost:3030/teams`, {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({
-    serializedGraph: team.save(),
-    teamKeyring: team.teamKeyring(),
-  }),
-})
+  const authProvider = new AuthProvider({ user, device, storage })
+
+  const repo = new Repo({
+    network: [authProvider.wrap(new BrowserWebSocketClientAdapter('ws://localhost:3030'))],
+    storage,
+  })
+
+  // create a team
+  const team = Auth.createTeam(`team ${name}`, context)
+  await authProvider.addTeam(team)
+
+  // get the server's public keys
+  const response = await fetch(`http://localhost:3030/keys`)
+  const keys = await response.json()
+
+  // add the server's public keys to the team
+  team.addServer({ host: 'localhost', keys })
+
+  // register the team with the server
+  await fetch(`http://localhost:3030/teams`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      serializedGraph: team.save(),
+      teamKeyring: team.teamKeyring(),
+    }),
+  })
+
+  repo.networkSubsystem.on('peer', payload => {
+    console.log(`${name} is connected`, payload)
+  })
+
+  return {
+    team,
+    authProvider,
+    repo,
+    context,
+  }
+}
 
 function App() {
   return 'hello world'
